401b7122d2176158e6b2962436656b2f5565148e
eMAD <emad.alam@yahoo.com>
2017-08-27 05:56:36 +0200

    :art: css to sass

--- src/options.js
+++ src/options.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles.css';
+import './styles/base.css';
 import SettingsContainer from './containers/Settings';
 
 ReactDOM.render(<SettingsContainer />, document.getElementById('root'));
--- src/tabpage.js
+++ src/tabpage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles.css';
+import './styles/base.css';
 import TabPageContainer from './containers/TabPage';
 
 ReactDOM.render(<TabPageContainer />, document.getElementById('root'));
