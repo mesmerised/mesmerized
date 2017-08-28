c5b068b147bdfe2204439dddd948331ef4b95bd4
eMAD <emad.alam@yahoo.com>
2017-08-28 22:48:34 +0200

    :zap: sample dynamic image import

--- src/modules/background/containers/Background.js
+++ src/modules/background/containers/Background.js
@@ -3,6 +3,24 @@ import FullscreenImage from '../components/FullscreenImage';
 import placeholderImage from '../images/placeholder.jpeg';
 import { getPhotoUrl, getRandomPrefetchedPhoto, prefetchRandomPhotos } from '../api';
 
+// @todo: make use of the dynamic imports
+// to store the local images path in localStorage
+// and later retrieve the path directly to be used
+// as a fallback image for the selected category
+/*
+import categories from '../configs/categories.config';
+
+Object.keys(categories).forEach(c => {
+    import(`../images/categories/${c}.json`)
+        .then(photos => {
+            photos.forEach(p => {
+                import(`../images/categories/${c}/${p.id}.jpg`)
+                    .then(x => console.log('image path', x))
+            });
+        });
+});
+*/
+
 // @todo: use local photos if nothing is available
 const photoUrl = getPhotoUrl(getRandomPrefetchedPhoto()) || placeholderImage;
 
