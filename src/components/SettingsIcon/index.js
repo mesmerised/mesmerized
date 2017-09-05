01e3ab7c467e1413e3e590f2be34a87d79c0fa09
eMAD <emad.alam@yahoo.com>
2017-09-05 21:09:56 +0200

    :art: settings icon transition

--- src/components/SettingsIcon/index.js
+++ src/components/SettingsIcon/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Tooltip from 'react-toolbox/lib/tooltip';
 import IconButton from 'react-toolbox/lib/button/IconButton';
+import './style.css';
 
 const IconButtonTooltip = Tooltip(IconButton);
 const openSettingsPage = () => chrome.runtime.openOptionsPage();
 
 const SettingsIcon = () => (
-    <IconButtonTooltip
-        tooltip="Settings"
-        icon="settings"
-        onClick={ openSettingsPage } />
+    <div className="settingsIcon">
+        <IconButtonTooltip
+            tooltip="Settings"
+            icon="settings"
+            onClick={ openSettingsPage } />
+    </div>
 );
 
 export default SettingsIcon;
