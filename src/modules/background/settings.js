66d07ae38bd42493224327379fe89cbe16f26346
eMAD <emad.alam@yahoo.com>
2017-09-09 23:00:49 +0200

    :art: Use settings factory for background settings

--- src/modules/background/settings.js
+++ src/modules/background/settings.js
@@ -1,48 +1,12 @@
-import * as StorageUtils from '@utils/storage.utils';
-import settingsConfigs from './configs/settings.config';
+import configs from './configs/settings.config';
+import { settingsFactory } from '@utils/settings.utils';
 
 /**
  * Default settings
  * @type {Object}
  */
-const DEFAULTS = {
+const defaults = {
     fetchFromServer: true,
 }
 
-/**
- * Returns the previously stored settings.
- * If no setting is present for the given key,
- * it returns the default value.
- *
- * @param  {String} key     The settings to get
- * @return {Mixed}          The settings value, or default
- */
-function getValue(key) {
-    const val = StorageUtils.get(settingsConfigs[key]);
-    if (typeof val === 'undefined' || val === null) {
-        return DEFAULTS[key];
-    }
-    return val;
-}
-
-/**
- * Stores the given settings in the appropriate key
- *
- * @param {String} key      The settings to store
- * @param {Mixed} value     The value to store
- */
-function setValue(key, value) {
-    StorageUtils.set(settingsConfigs[key], value);
-}
-
-/**
- * List of available settings
- */
-export default {
-    get fetchFromServer() {
-        return getValue('fetchFromServer');
-    },
-    set fetchFromServer(value) {
-        setValue('fetchFromServer', value);
-    }
-}
+export default settingsFactory({ configs, defaults });
