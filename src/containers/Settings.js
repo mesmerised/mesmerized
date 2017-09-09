31517b7b52a62ed1127b7139cbda1a4c48d07bd8
eMAD <emad.alam@yahoo.com>
2017-09-09 23:02:48 +0200

    :zap: Add Clock settings to the list of settings

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Layout from 'react-toolbox/lib/layout/Layout';
 import Panel from 'react-toolbox/lib/layout/Panel';
 import { Settings as BackgroundSettings } from '@modules/background';
+import { Settings as ClockSettings } from '@modules/clock';
 
 class Settings extends Component {
     render() {
@@ -11,6 +12,9 @@ class Settings extends Component {
                     <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
                         <section>
                             <BackgroundSettings />
+                        </section>
+                        <section>
+                            <ClockSettings />
                         </section>
                     </div>
                 </Panel>
