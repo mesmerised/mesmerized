4fd3d4909d942f5a4d4a0d351fea0d87aeabe0c3
eMAD <emad.alam@yahoo.com>
2017-09-14 00:18:46 +0200

    :art: Remove settings icon tooltip

--- src/components/SettingsIcon/index.js
+++ src/components/SettingsIcon/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import Tooltip from 'react-toolbox/lib/tooltip';
 import IconButton from 'react-toolbox/lib/button/IconButton';
 import './style.css';
 
-const IconButtonTooltip = Tooltip(IconButton);
 const openSettingsPage = () => chrome.runtime.openOptionsPage();
 
 const SettingsIcon = () => (
     <div className="settingsIcon">
-        <IconButtonTooltip
-            tooltip="Settings"
+        <IconButton
             icon="settings"
             onClick={ openSettingsPage } />
     </div>
