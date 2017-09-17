6727ee22e99abea315ed506d588ba6f305311941
eMAD <emad.alam@yahoo.com>
2017-09-18 00:28:01 +0200

    :art: Common css for control icons

--- src/components/SettingsIcon/index.js
+++ src/components/SettingsIcon/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import IconButton from 'react-toolbox/lib/button/IconButton';
-import './style.css';
 
 const openSettingsPage = () => chrome.runtime.openOptionsPage();
 
 const SettingsIcon = () => (
-    <div className="settingsIcon">
+    <div className="controlIcon">
         <IconButton
             icon="settings"
             onClick={ openSettingsPage } />
