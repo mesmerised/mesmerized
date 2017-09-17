b40fc86a551df82c4da6ab6b61c99439d342b60c
eMAD <emad.alam@yahoo.com>
2017-09-17 22:10:55 +0200

    :zap: Auto generate Settings page

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -3,11 +3,35 @@ import Layout from 'react-toolbox/lib/layout/Layout';
 import Panel from 'react-toolbox/lib/layout/Panel';
 import List from 'react-toolbox/lib/list/List';
 import ListDivider from 'react-toolbox/lib/list/ListDivider';
-import { Settings as BackgroundSettings } from '@modules/background';
-import { Settings as ClockSettings } from '@modules/clock';
-import { Settings as QuotesSettings } from '@modules/quotes';
-import { Settings as WeatherSettings } from '@modules/weather';
+import * as Modules from '@modules';
 import './Settings.css';
+
+const settingsContainersKeys = Object.keys(Modules);
+const settingsContainersKeysLastIndex = settingsContainersKeys.length - 1;
+
+
+// iterate and generate an array of module settings
+// container separated by the `ListDivider`
+// eg:
+//  settingsContainerItems = [
+//      <BackgroudSettings />
+//      <ListDivider />
+//      <ClockSettings />
+//      <ListDivider />
+//      <QuotesSettings />
+//  ]
+const settingsContainerItems = settingsContainersKeys.reduce((arr, m, index) => {
+    const module = Modules[m];
+    const SettingsContainer = module.Settings;
+
+    SettingsContainer && arr.push(<SettingsContainer key={ index } />);
+    if (index !== settingsContainersKeysLastIndex) {
+        const dividerKey = `${index}-divider`;
+        arr.push(<ListDivider key={ dividerKey } />);
+    }
+
+    return arr;
+}, []);
 
 class Settings extends Component {
     render() {
@@ -15,13 +39,7 @@ class Settings extends Component {
             <Layout>
                 <Panel>
                     <List selectable ripple>
-                        <BackgroundSettings />
-                        <ListDivider />
-                        <QuotesSettings />
-                        <ListDivider />
-                        <ClockSettings />
-                        <ListDivider />
-                        <WeatherSettings />
+                        { settingsContainerItems }
                     </List>
                 </Panel>
             </Layout>
---
+++ src/modules/index.js
@@ -0,0 +1,11 @@
+import * as Background from './background';
+import * as Clock from './clock';
+import * as Quotes from './quotes';
+import * as Weather from './weather';
+
+export {
+    Background,
+    Clock,
+    Quotes,
+    Weather,
+};
