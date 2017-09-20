30008d6ed6adf11b1851ca7f9d17f7781dda6bda
eMAD <emad.alam@yahoo.com>
2017-09-21 00:18:46 +0200

    :wrench: Fix fonts import to reduce build size

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'material-design-icons/iconfont/material-icons.css';
 import './assets/react-toolbox/theme.css';
 import './assets/styles/base.css';
