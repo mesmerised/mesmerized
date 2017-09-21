6c40f542f73c655ea4e620b5e66635e864d1b851
eMAD <emad.alam@yahoo.com>
2017-09-22 00:07:20 +0200

    :art: Update Weather module icon set

--- src/modules/weather/components/Weather/index.js
+++ src/modules/weather/components/Weather/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './style.css'
 
-const imageBaseUrl = 'https://openweathermap.org/img/w';
-
 const Weather = ({ temperature, cityName, iconId, isLoading }) => (
     <div className={ isLoading ? "weather weather__loading" : "weather" }>
         { temperature &&
             <div className="weather__temperature">{ Math.round(temperature) }</div>
         }
         { iconId &&
-            <img src={`${imageBaseUrl}/${iconId}.png`} className="weather__image" alt="" />
+            <i className={`weather__icon weather__icon_${iconId}`} />
         }
         { cityName &&
             <div className="weather__cityName">{ cityName }</div>
