46cb90049d67a8a105a4e42aa102945cc6d54f48
eMAD <emad.alam@yahoo.com>
2017-09-25 02:24:03 +0200

    :bug: Add missing weather module setting config key

--- src/modules/weather/configs/settings.config.js
+++ src/modules/weather/configs/settings.config.js
@@ -1,6 +1,7 @@
 import { MODULE_NAME } from './constants';
 
 export default {
+    showWeather: `${MODULE_NAME}.settings.weather.show`,
     unit: `${MODULE_NAME}.settings.weather.unit`,
     refreshInterval: `${MODULE_NAME}.settings.weather.refreshInterval`,
 };
