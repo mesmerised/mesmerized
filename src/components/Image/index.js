f3404a43c337b826684c0956ac38767b4e4bb210
eMAD <emad.alam@yahoo.com>
2017-09-26 19:09:22 +0200

    :racehorse: Fix memory leak in Image component prefetch

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -8,53 +8,73 @@ class Image extends Component {
         alt: '',
         placeholder: placeholderImg,
         extraClasses: '',
-        onLoad: x => x,
-        onError: x => x
     };
 
     state = {
+        prefetchedSource: placeholderImg,
         isLoaded: false,
         isError: false
     };
 
-    componentDidMount() {
-        const {
-            src,
-            timeout,
-            onLoad,
-            onError
-        } = this.props;
+    prefetchImage = () => {
+        const { src, timeout } = this.props;
 
         prefetch(src, timeout)
-            .then(img => {
-                this.setState({
-                    isLoaded: true,
-                    isError: false
-                });
-                onLoad(img);
-            })
-            .catch(img => {
-                this.setState({
-                    isLoaded: false,
-                    isError: true
-                });
-                onError(img);
-            });
+            .then(this.handlePrefetchSuccess)
+            .catch(this.handlePrefetchError);
+    };
+
+    handlePrefetchSuccess = (img) => {
+        if (!this._mounted) return;
+
+        const { src, onLoad } = this.props;
+
+        this.setState({
+            prefetchedSource: src,
+            isLoaded: true,
+            isError: false,
+        });
+
+        onLoad && onLoad(img);
+    };
+
+    handlePrefetchError = (img) => {
+        if (!this._mounted) return;
+
+        const { onError, placeholder } = this.props;
+
+        this.setState({
+            prefetchedSource: placeholder,
+            isLoaded: true,
+            isError: false,
+        });
+
+        onError && onError(img);
+    };
+
+    componentWillUnmount() {
+        this._mounted = false;
+    }
+
+    componentDidMount() {
+        this._mounted = true;
+        this.prefetchImage();
     }
 
     render() {
         const {
-            src,
             alt,
             placeholder,
             extraClasses
         } = this.props;
+
         const {
+            prefetchedSource,
             isLoaded,
             isError
         } = this.state;
 
-        const imageSource = isLoaded ? src : placeholder;
+        const imageSource = isLoaded ? prefetchedSource : placeholder;
         const isLoadingClass = !isLoaded && !isError ? 'image__loading' : '';
 
         return (
