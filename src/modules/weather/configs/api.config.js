b7ed81c8a19142d97ae4bb24d391e7971ccabefb
eMAD <emad.alam@yahoo.com>
2018-06-08 13:05:55 +0200

    :card_index: Update security policies

--- src/modules/weather/configs/api.config.js
+++ src/modules/weather/configs/api.config.js
@@ -3,7 +3,7 @@
  */
 export default {
     appid: 'b14ad9cd7267f656c112a5edac47680b',
-    base: 'http://api.openweathermap.org/data/2.5',
+    base: 'https://api.openweathermap.org/data/2.5',
     uris: {
         current: '/weather/'
     },
